fef9e5c2b1de8aad06f81d6783c8c977c96f51f7
kelco-chan <60736592+kelco-chan@users.noreply.github.com>
2022-04-25 12:20:29 +1000

    chore(**/*.js): Remove javascript files from repo

--- assets/colors.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Colors = void 0;
-exports.Colors = {
-    error: 0xf44336,
-    success: 0x00ab66
-};
-//# sourceMappingURL=colors.js.map
